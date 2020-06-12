dc4278d82161a8ca2be3d8cd4320107127434835
Dave Lilly <dave.lilly@commercetools.de>
2020-06-12 13:53:06 -0500

    Added expired flag

--- scripts/delete-all-categories.js
+++ scripts/delete-all-categories.js
@@ -1,23 +1,20 @@
 const _ = require('lodash')
+const log = require('single-line-log').stdout
+
 const CT = require('..')
 
 let sleep = async(ms) => { return new Promise(resolve => setTimeout(resolve, ms)); }
 let run = async () => {
-    const ct = await CT.getClient();
     try {
-        let deleteCategory = async category => {
-            console.log(`delete category [ ${category.id} ]...`)
+        const ct = await CT.getClient()
+        await ct.categories.process(async category => {
+            log(`Deleting category [ ${category.key} ]...`)
+            await ct.categories.delete(category)
             await sleep(200)
-            return await ct.categories.delete(category)
-        }
-        let processCategory = async payload => _.each(payload.body.results, deleteCategory)
-        
-        await ct.categories.process(processCategory)
+        })
     } catch (error) {
         console.error(`Error: ${error}`)
-    } finally {
-        process.exit(0)
-    }  
+    }
 }
 
 run()
